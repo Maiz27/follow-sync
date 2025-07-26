ff092ace14d93b93afd4a0b566987f9b3a442ce3
Maged Faiz <magedfaiz98@gmail.com>
2025-07-26 16:50:04 +0200

    Add next-themes for theme support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import { ThemeProvider } from 'next-themes';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -12,8 +13,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className=''>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
