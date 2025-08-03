99bad1c17b24afa13048f3ba899a2706cf1a5fe6
Maged Faiz <magedfaiz98@gmail.com>
2025-08-03 14:49:58 +0200

    fix: theme dynamic logo render

--- src/components/nav/logo.tsx
+++ src/components/nav/logo.tsx
@@ -1,31 +1,24 @@
-'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Image from 'next/image';
-import { useTheme } from 'next-themes';
 
 const Logo = () => {
-  const { theme, resolvedTheme } = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
-
-  const isDark = theme === 'dark' || resolvedTheme === 'dark';
-
   return (
-    <div>
+    <>
       <Image
-        src={isDark ? '/imgs/logo/logo-w.png' : '/imgs/logo/logo-b.png'}
+        src={'/imgs/logo/logo-b.png'}
         alt='Follow Sync Logo'
         width={30}
         height={30}
+        className='block dark:hidden'
       />
-    </div>
+      <Image
+        src={'/imgs/logo/logo-w.png'}
+        alt='Follow Sync Logo'
+        width={30}
+        height={30}
+        className='hidden dark:block'
+      />
+    </>
   );
 };
 
