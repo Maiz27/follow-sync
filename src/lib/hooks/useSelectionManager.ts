185ec8453e761c81800055eec3331aa114114115
Maged Faiz <magedfaiz98@gmail.com>
2025-08-11 17:19:18 +0200

    feat: Create useSelectionManager hook

---
+++ src/lib/hooks/useSelectionManager.ts
@@ -0,0 +1,44 @@
+import { useState, useMemo } from 'react';
+
+export const useSelectionManager = (itemIds: string[] = []) => {
+  const [selectedIds, setSelectedIds] = useState(new Set<string>());
+
+  const handleSelect = (id: string) => {
+    setSelectedIds((prev) => {
+      const newSet = new Set(prev);
+      if (newSet.has(id)) {
+        newSet.delete(id);
+      } else {
+        newSet.add(id);
+      }
+      return newSet;
+    });
+  };
+
+  const handleSelectAll = () => {
+    setSelectedIds((prev) => {
+      if (prev.size === itemIds.length) {
+        return new Set<string>();
+      } else {
+        return new Set(itemIds);
+      }
+    });
+  };
+
+  const clearSelection = () => {
+    setSelectedIds(new Set<string>());
+  };
+
+  const isAllSelected = useMemo(
+    () => itemIds.length > 0 && selectedIds.size === itemIds.length,
+    [selectedIds, itemIds]
+  );
+
+  return {
+    selectedIds,
+    handleSelect,
+    handleSelectAll,
+    clearSelection,
+    isAllSelected,
+  };
+};
